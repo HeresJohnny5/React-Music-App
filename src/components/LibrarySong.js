2f071f7a6898e75da3d7e60b22b64f8c2b2ca057
HeresJohnny5 <ericksonjohn01@hotmail.com>
2021-01-05 21:56:06 -0500

    Refine LibrarySong HTML structure and work on Library styles.

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -2,10 +2,12 @@ import React from "react";
 
 export const LibrarySong = ({ song }) => {
   return (
-    <div className="library-container">
+    <div className="library-song">
       <img src={song.cover} alt={song.name} />
-      <h3>{song.audio}</h3>
-      <h4>{song.artist}</h4>
+      <div className="song-description">
+        <h3>{song.name}</h3>
+        <h4>{song.artist}</h4>
+      </div>
     </div>
   );
 };
